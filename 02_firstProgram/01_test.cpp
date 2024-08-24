173cb245f80ff54adfaaee16113f27f431d05e60
parthmern <parthmern@gmail.com>
2024-08-24 05:54:27 -0400

    test

---
+++ 02_firstProgram/01_test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+    cout << "hello" << endl;
+    int a; 
+    cin >> a;
+    
+    if(a>0){
+        cout << "positive" << endl;
+    }
+    else{
+        cout << "neg" << endl;
+    }
+
+    return 0 ;
+}
