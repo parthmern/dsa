05a16cded066f888dad2c16a77e971da2058199f
parthmern <parthmern@gmail.com>
2024-08-24 13:40:16 -0400

    tc

--- 02_firstProgram/03_typeConversion.cpp
+++ 02_firstProgram/03_typeConversion.cpp
@@ -3,11 +3,18 @@ using namespace std;
 
 int main(){
 
-    char ch = 97 ;
+    // implicit 
+    char ch = 97 ;           // giving int value but converting and storing into character value
     cout << ch << endl;      // output : a
 
-    int num = 'b';
+    int num = 'b';           // giving char value but converting and storing into int value
     cout << num << endl;     // output : 98
 
+
+    // explicit
+    double d = 5.55;
+    int ans = (int) d;       // double d is converting into int explicitly and assign to int ans
+    cout << ans << endl;     // output : 5
+ 
     return 0;
 }
