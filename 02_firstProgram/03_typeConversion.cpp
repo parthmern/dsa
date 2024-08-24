e5e0ca4e5bf9188b1f8d400030ece5bb70df0c49
parthmern <parthmern@gmail.com>
2024-08-24 13:32:22 -0400

    typeconversion

---
+++ 02_firstProgram/03_typeConversion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+
+    char ch = 97 ;
+    cout << ch << endl;      // output : a
+
+    int num = 'b';
+    cout << num << endl;     // output : 98
+
+    return 0;
+}
