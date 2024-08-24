2a0adfed666fefca343d94bd4f12654f0bb89f26
parthmern <parthmern@gmail.com>
2024-08-24 06:45:47 -0400

    datatypes

---
+++ 02_firstProgram/02_datatype.cpp
@@ -0,0 +1,12 @@
+// bool = 1 byte because 
+// 1 bit is sufficient but we cannot address 1 bit
+// we need at least 1 byte to make it addressable using variable name
+
+// 1 byte = 8 bit
+// float = 4 byte ==> for small numbers
+// double = 8 byte
+
+// int = 4 byte = 32 bits => 2^32 - 1 = is becoming the max number that we can store into int
+
+// char = 1 byte = 8 bit = 2^8 - 1 = 255 max can store to 255
+// char name = 256 // overflow ho jayega
