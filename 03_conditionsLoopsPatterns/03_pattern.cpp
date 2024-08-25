b65d5286e32389106a7af25abfa4a5063b0b9fa7
parthmern <parthmern@gmail.com>
2024-08-25 09:03:47 -0400

    pyramid pattern

--- 03_conditionsLoopsPatterns/03_pattern.cpp
+++ 03_conditionsLoopsPatterns/03_pattern.cpp
@@ -1,23 +1,69 @@
+
+//           *
+//         *   *
+//       *   *   *
+//     *   *   *   *
+//   *   *   *   *   *
+// *   *   *   *   *   *
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int rows = 6;  // Number of rows in the pyramid
+
+//     for (int i = 0; i < rows; i++) {
+//         // Print leading spaces
+//         for (int j = 0; j < (rows - i - 1) * 2; j++) {
+//             cout << " ";
+//         }
+
+//         // Print stars
+//         for (int k = 0; k <= i; k++) {
+//             cout << "*";
+//             if (k < i) {
+//                 cout << "   ";  // 3 spaces between stars
+//             }
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
+
+// ========================================================
+
+//      *     
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+
 #include <iostream>
-using namespace std;
+using namespace std; 
 
-int main() {
-    int rows = 6;  // Number of rows in the pyramid
+int main(){
 
-    for (int i = 0; i < rows; i++) {
-        // Print leading spaces
-        for (int j = 0; j < (rows - i - 1) * 2; j++) {
-            cout << " ";
-        }
+    int N = 6;
 
-        // Print stars
-        for (int k = 0; k <= i; k++) {
-            cout << "*";
-            if (k < i) {
-                cout << "   ";  // 3 spaces between stars
+    for(int i=0; i<N; i++){
+
+        for(int j=0; j < ((2*N)-1) ; j++){
+            
+            if(j< N-i-1){
+                cout << " ";
+            }
+            else if(j < i+N){
+                cout << "*";
+            }
+            else{
+                cout << " ";
             }
         }
-        cout << endl;
+
+        cout << endl ;
     }
 
     return 0;
