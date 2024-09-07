bfebf45f5e8cc7c501ce4fef465d64609bcfc5f1
parthmern <parthmern@gmail.com>
2024-09-07 06:33:23 -0400

    solid

--- 04_patterns/11_solidHalfDiamond.cpp
+++ 04_patterns/11_solidHalfDiamond.cpp
@@ -1,4 +1,17 @@
-int main(){
+//*
+//**
+//*** 
+//****
+//*****
+//****
+//*** 
+//**
+//*
+
+#include <iostream>
+using namespace std ;
+
+// int main(){
 
 //     int n=5;
 
@@ -23,3 +36,21 @@ int main(){
 //     return 0;
 
 // }
+
+
+// short code
+
+int main(){
+    int n=5;
+    
+    for(int i=0; i<2*n-1; i++){
+        int cond = i<n ? i : n-(i%n)-2;
+
+        for(int j=0; j<=cond; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
