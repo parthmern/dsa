2cec9f52648856886edb4010f3f72467ac35c220
parthmern <parthmern@gmail.com>
2024-09-08 07:00:28 -0400

    updt

--- 04_patterns/18_numericFullPyramid.cpp
+++ 04_patterns/18_numericFullPyramid.cpp
@@ -42,3 +42,17 @@ int main(){
 
     return 0;
 }
+
+
+// =========================
+
+//         1 | 
+//       2 3 |2
+//     3 4 5 |4 3
+//   4 5 6 7 |6 5 4
+// 5 6 7 8 9 |8 7 6 5
+
+// for pyramid after | line
+// u can also make logic like
+// loop run til noOfRow
+// print start from 2*row and then do decrement
