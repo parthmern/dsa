baebe22cd0e9ef6b7011ab302a293e6727a3f54c
parthmern <parthmern@gmail.com>
2024-09-18 03:49:33 -0400

    find

--- 09_array/21_findDuplicateNum.cpp
+++ 09_array/21_findDuplicateNum.cpp
@@ -45,14 +45,34 @@ int negMarkingMethodDuplicateEle(vector<int>& nums){
 }
 
 
+// POSITIONING METHOD
+// POSITIONING METHOD (Cyclic Sort approach)
+int positionMethodDuplicate(vector<int>& nums) {
+    while (nums[0] != nums[nums[0]]) {
+        // Swap nums[0] with the element at its correct position nums[nums[0]]
+        swap(nums[0], nums[nums[0]]);
+    }
 
+    return nums[0]; // The duplicate number
+}
 
 int main() {
     vector<int> arr{1, 3, 4, 2, 2};
 
-    int duplicate = duplicateEle(arr);
-    int dup = negMarkingMethodDuplicateEle(arr);
-    cout << "Duplicate element is: " << duplicate << "---" << dup << endl;
+    // raw array performs shallow copy (error not allowed)
+    // int arr1[] = {1, 2, 3};
+    // int arr2[] = {4, 5, 6};
+    // arr2 = arr1; // This is not allowed in C++
+
+    //performs a deep copy by vector
+    vector<int> arr1 = arr;  //  Deep copy for Sorting Method
+    vector<int> arr2 = arr;  // Deep copy for Negative Marking Method
+    vector<int> arr3 = arr;  // Deep copy for Positioning Method
+
+    int duplicate = duplicateEle(arr1);
+    int dup = negMarkingMethodDuplicateEle(arr2);
+    int dupp = positionMethodDuplicate(arr3);
+    cout << "Duplicate element is: " << duplicate << "---" << dup << "---" << dupp << endl;
 
     
     return 0;
