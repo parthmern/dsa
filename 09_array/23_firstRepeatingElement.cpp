84a5cf71623483d05544dff3f1e3af2217b32c0c
parthmern <parthmern@gmail.com>
2024-10-02 03:38:32 -0400

    hashed

--- 09_array/23_firstRepeatingElement.cpp
+++ 09_array/23_firstRepeatingElement.cpp
@@ -1,31 +1,23 @@
-
-
 #include <iostream>
-#include <unordered_map> 
+#include <unordered_map>
 using namespace std;
 
-int main(){ 
+// Function to count the frequency and find the first duplicate
+void findFirstDuplicate(int arr[], int size) {
+    unordered_map<int, int> hash;
 
-    int arr[]= {1, 5, 3, 4, 3, 5, 6};
-    int size = sizeof(arr)/sizeof(arr[0]);
-
-    unordered_map<int, int> hash; // [int, int]
-
-    for(int i=0; i<size; i++){
-
-        if(hash[arr[i]] == 1){
+    // First loop: Count the frequency of each element
+    for (int i = 0; i < size; i++) {
+        if (hash[arr[i]] == 1) {
             hash[arr[i]]++;
-        }else{
-            // initalize with 1
-            hash[arr[i]] = 1 ;   
+        } else {
+            hash[arr[i]] = 1;
         }
-
     }
 
     // Print the frequency of each element
     // for(auto& pair : hash){
     //     cout << "Element " << pair.first << " appears " << pair.second << " times." << endl;
-        
     // }
     
     // Element 6 appears 1 times.
@@ -34,20 +26,45 @@ int main(){
     // Element 3 appears 2 times.
     // Element 4 appears 1 times.
 
+    // Second loop: Find the first element that has appeared more than once
+    for (int i = 0; i < size; i++) {
+        if (hash[arr[i]] > 1) {
+            cout << "index+1 => " << i + 1 << " and element is " << arr[i] << endl;
+            break; // Stop after finding the first duplicate
+        }
+    }
+}
 
-    // traversing and check
-    for(int i=0; i<size; i++){
+void findFirstDuplicatewithOwnhashingWay(int arr[], int size){
 
-        if(hash[arr[i]] > 1){
-            // means duplicate availabe 2 times appear
-            cout << "index+1=>" << i+1 << " and ele is " << arr[i] << endl;
-            break;
+    int hash[7] = {0};
+
+    for (int i = 0; i < size; i++) {
+        
+        for(int j=0; j<size; j++){
+            if(i == arr[j]){
+                hash[i] = hash[i] + 1;
+            }
         }
 
     }
 
-    
+    for(int i=0; i<size; i++){
+        
+        if(hash[arr[i]] > 1){
+            cout << "index=> " << hash[arr[i]]  << " and element is " << arr[i] << endl;
+            break;
+        }
+    }
+}
 
+int main() {
+    int arr[] = {1, 5, 3, 4, 3, 5, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
-    
+    findFirstDuplicate(arr, size);
+
+    findFirstDuplicatewithOwnhashingWay(arr, size);
+
+    return 0;
 }
