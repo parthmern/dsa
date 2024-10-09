5f164183720c58e59db1e5a554502b6ed2d30d8f
parthmern <parthmern@gmail.com>
2024-10-09 08:00:56 -0400

    add two num in array

---
+++ 09_array/27_addTwoNumRepresentedByTwoArray.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> // for reverse()
+using namespace std;
+
+int main() {
+    int a[] = {9, 3, 9};
+    int b[] = {6, 1};
+
+    int n = sizeof(a) / sizeof(a[0]);
+    int m = sizeof(b) / sizeof(b[0]);
+
+    int i = n - 1;
+    int j = m - 1;
+
+    vector<int> ans;
+    int carry = 0;
+
+    // Process both arrays while both have digits left
+    while (i >= 0 && j >= 0) {
+        cout << "inner: i = " << i << ", j = " << j << endl;
+
+        int sum = a[i] + b[j] + carry;
+        int digit = sum % 10;
+        carry = sum / 10;
+
+        ans.push_back(digit);
+
+        i--;
+        j--;
+    }
+
+    // If a[] has remaining digits
+    while (i >= 0) {
+        cout << "inner two-> i = " << i << endl;
+
+        int sum = a[i] + carry;
+        int digit = sum % 10;
+        carry = sum / 10;
+
+        ans.push_back(digit);
+
+        i--;
+    }
+
+    // If b[] has remaining digits
+    while (j >= 0) {
+        cout << "inner three-> j = " << j << endl;
+
+        int sum = b[j] + carry;
+        int digit = sum % 10;
+        carry = sum / 10;
+
+        ans.push_back(digit);
+
+        j--;
+    }
+
+    // If there's any remaining carry
+    if (carry) {
+        cout << "remaining carry: " << carry << endl;
+        ans.push_back(carry);
+    }
+
+    // Reverse the result since it's stored in reverse order
+    reverse(ans.begin(), ans.end());
+
+    // Print the result
+    cout << "Sum: ";
+    for (int digit : ans) {
+        cout << digit;
+    }
+    cout << endl;
+
+    return 0;
+}
