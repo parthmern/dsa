2a87ae0a272c9635134666a7cdaaa13e5b4f4403
parthmern <parthmern@gmail.com>
2024-10-16 09:07:55 -0400

    rmeove all adj

---
+++ 11_charArrays-String/02_removeAllAdjacentDuplicates.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string removeDuplicates(string s) {
+    string ans = "";
+
+    for (int i = 0; i < s.length(); i++) {
+        if (ans.length() > 0) {
+            if (ans[ans.length() - 1] == s[i]) {
+                ans.pop_back();
+            } else {
+                ans.push_back(s[i]);
+            }
+        } else {
+            ans.push_back(s[i]);
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    string input = "abbaca";
+    // cout << "Enter a string: ";
+    // cin >> input;
+
+    string result = removeDuplicates(input);
+    cout << "String after removing adjacent duplicates: " << result << endl;
+
+    return 0;
+}
