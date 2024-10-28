48b72913fa3e79f90853374053079e5ac8b24028
parthmern <parthmern@gmail.com>
2024-10-28 17:09:50 -0400

    climbing stairs

---
+++ 13_recursion/04_climbStairs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int climbStairs(int n){
+
+    // base case
+    if(n==0) return 1;
+    if(n==1) return 1;
+
+    int ans = climbStairs(n-1)+climbStairs(n-2);
+
+    return ans;
+
+}
+
+int main(){
+
+    cout << climbStairs(5);
+
+    return 0;
+}
