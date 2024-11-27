7addc0a5a6fcf1ccfa95150013c904c4e31676a3
parthmern <parthmern@gmail.com>
2024-11-27 09:21:46 -0500

    merge inplace

--- 14_DnC/02_quickSort.cpp
+++ 14_DnC/02_quickSort.cpp
@@ -41,29 +41,42 @@ int partition(int *arr, int s, int e){
     // }
 
 
-    vector<int> smaller, greater;
+    // vector<int> smaller, greater;
 
-    for (int i = s + 1; i <= e; i++) {
-        if (arr[i] <= pivotEle) {
-            smaller.push_back(arr[i]);
-        } else {
-            greater.push_back(arr[i]);
+    // for (int i = s + 1; i <= e; i++) {
+    //     if (arr[i] <= pivotEle) {
+    //         smaller.push_back(arr[i]);
+    //     } else {
+    //         greater.push_back(arr[i]);
+    //     }
+    // }
+
+    // // Rebuild the array with smaller, pivot, and greater elements
+    // int index = s;
+    // for (int num : smaller) {
+    //     arr[index++] = num;
+    // }
+
+    // pivotIndex = index; // Correct position of pivot
+    // arr[index++] = pivotEle;
+
+    // for (int num : greater) {
+    //     arr[index++] = num;
+    // }
+
+    while(i==j){
+
+        if( arr[i] > pivotEle ){
+            swap(arr[i], arr[j]);
+            j--;
         }
+
+        if(arr[i] <= pivotEle){
+            swap(arr[i], arr[j]);
+            i++;
+        }
+
     }
-
-    // Rebuild the array with smaller, pivot, and greater elements
-    int index = s;
-    for (int num : smaller) {
-        arr[index++] = num;
-    }
-
-    pivotIndex = index; // Correct position of pivot
-    arr[index++] = pivotEle;
-
-    for (int num : greater) {
-        arr[index++] = num;
-    }
-
 
 
     return pivotIndex;
---
+++ 14_DnC/09_mergeSortInPlace.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+void mergeInPlace(int *arr, int s, int e){
+    
+    int totalLength = (e-s) + 1;
+    int gap = (totalLength/2) + (totalLength%2);
+
+    while (gap>0)
+    {
+        int i = s;
+        int j = s+gap;
+
+        while(j<=e){
+            if(arr[i]>arr[j]){
+                swap(arr[i],arr[j]);
+            }
+            i++;
+            j++;
+
+        }
+        
+        // gap agar 1 hai to usko break karne k liye 0 de diya
+        gap = gap<=1 ? 0 : ((gap/2) + (gap%2));
+
+    }
+    
+
+}
+
+void mergeSort(int *arr, int s, int e){
+
+    // base case
+    if(s==e){
+        // single element
+        return;
+    }
+
+    // rr
+    int mid = (s+e)/2;
+    // left part sort 
+    mergeSort(arr ,s, mid);
+
+    // right part sort
+    mergeSort(arr, mid+1, e);
+
+    // merge two sorted arr
+    mergeInPlace(arr,s,e);
+
+}
+
+int main(){
+
+    int arr[] = {4,5,13,2,12};
+
+    int n = 5;
+    
+    int s = 0;
+    int e = n-1;
+
+    mergeSort(arr,s,e);
+
+    for(int i=0; i<n;i++){
+        cout << arr[i] << " ";
+    }
+
+
+    return 0;
+}
