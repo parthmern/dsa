7b6a9555072fbf16d4a4d2f60a3ccd55d8828f52
parthmern <parthmern@gmail.com>
2025-09-16 21:30:11 -0400

    completeness of bt

---
+++ 21_Heap/09_completenessOfBt.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/check-completeness-of-a-binary-tree/description/ 
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        if (!root) return true;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        bool nullSeen = false;
+
+        while (!q.empty()) {
+            TreeNode* curr = q.front();
+            q.pop();
+
+            if (curr == NULL) {
+                nullSeen = true;  // once we see NULL, mark it
+            } else {
+                if (nullSeen) {
+                    // If we already saw NULL and now see a node → not complete
+                    return false;
+                }
+                q.push(curr->left);
+                q.push(curr->right);
+            }
+        }
+        return true;
+    }
+};
