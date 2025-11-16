3ab79494dabdc1d697076dd41a4b3400296d3641
parthmern <parthmern@gmail.com>
2025-11-16 08:54:22 -0500

    longest valid paranthesis

---
+++ Z_practise/100_longestValidParanthesis.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+
+        Stack<Integer> st = new Stack<>();
+        int maxLen = 0;
+
+        st.push(-1);
+
+        for( int i=0; i<s.length() ; i++ ){
+
+            char ch = s.charAt(i);
+
+            if( ch == '(' ){
+                st.push(i);
+            }else{
+                if(!st.empty())  st.pop();
+               
+                if( st.empty() ){
+                    st.push(i);
+                }else{
+                    maxLen = Math.max( maxLen, i - st.peek() );
+                }
+            }
+        }
+
+        return maxLen;
+        
+    }
+}
