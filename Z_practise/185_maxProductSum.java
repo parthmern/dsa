90c625e12ab3f2b6d0d59e4d2c12638ef73b73b8
parthmern <parthmern@gmail.com>
2026-01-11 12:51:31 -0500

    max prod sum

---
+++ Z_practise/185_maxProductSum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        
+        int maxi = Integer.MIN_VALUE;
+        int n = nums.length;
+
+        int prefix = 1;
+        int suffix = 1;
+
+        for(int i=0; i<nums.length; i++){
+
+            if(prefix==0) prefix=1;
+            if(suffix==0) suffix=1;
+
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[n-i-1];
+
+            maxi = Math.max(maxi, Math.max(prefix, suffix));
+
+        }
+
+        return maxi;
+    }
+}
