d60f3a281dfbcc8cdcf3970fa38e7b1935dee6fb
parthmern <parthmern@gmail.com>
2026-01-20 21:48:23 -0500

    long palindromic py

---
+++ Z_practise/209_longestPalindromic.py
@@ -0,0 +1,12 @@
+def reverse(s: str) -> str:
+    return s[::-1]   # slice-based reverse
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        ans = ""
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                substring = s[i:j+1]
+                if substring == reverse(substring) and len(substring) > len(ans):
+                    ans = substring
+        return ans
