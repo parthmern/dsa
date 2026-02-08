4cda56255d031b7af86fd4d821e9e1e0174969e0
parthmern <parthmern@gmail.com>
2026-02-08 16:54:02 -0500

    asteroidCollision

---
+++ Z_practise/331_asteroidCollision.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+
+        Stack<Integer> st = new Stack<>();
+
+        for(int a : asteroids){
+
+            boolean destroyed = false;
+
+            while(!st.isEmpty() && a < 0 && st.peek() > 0){
+
+                if(st.peek() < -a){
+                    st.pop();    
+                    continue;
+                }
+                else if(st.peek() == -a){
+                    st.pop();    // both same remove both
+                }
+
+                destroyed = true;
+                break;
+            }
+
+            if(!destroyed){
+                st.push(a);
+            }
+        }
+
+        int[] res = new int[st.size()];
+        for(int i = res.length-1; i >= 0; i--){
+            res[i] = st.pop();
+        }
+
+        return res;
+    }
+}
