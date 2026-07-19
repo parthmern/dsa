b9516136535594510ad98fad22a58fefcfc04070
parthmern <parthmern@gmail.com>
2026-07-19 11:51:17 -0400

    split arr largest  sum

---
+++ Z_practise/354_splitarr.java
@@ -0,0 +1,75 @@
+class Solution {
+
+            private long howManyBooks(int[] arr, int days, long mid){
+
+        long ans = 0;
+        long prex = 0;
+        for(int ele: arr){
+
+            prex+=ele;
+            
+            if(prex>mid){
+                ans++;
+                prex = ele;
+            }
+
+            if(mid == prex){
+                ans++;
+                prex = 0;
+            }
+            
+
+        }
+
+        if(prex!=0 && prex <= mid) ans++;
+
+        return ans;
+
+    }
+    
+    public int findPages(int[] arr, int k) {
+        
+        if (k > arr.length) {
+            return -1;
+        }
+
+        // code here
+        
+        long  mini = Integer.MAX_VALUE;
+        long sum = 0;
+
+        long maxi = Integer.MIN_VALUE;
+
+        for(int ele: arr){
+            mini = Math.min(mini, ele);
+            maxi = Math.max(maxi, ele);
+            sum+=ele;
+        }
+
+        long s = maxi;
+        long e = sum;
+
+
+        long res = 0;
+
+        while (s <= e) {
+            long mid = s + (e - s) / 2;
+            long students = howManyBooks(arr, k, mid);
+        
+            if (students <= k) {
+                res = mid;
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+
+        return (int)res;
+        
+    }
+    
+
+    public int splitArray(int[] nums, int k) {
+        return this.findPages(nums, k);
+    }
+}
